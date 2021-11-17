ae3e8967cf9b91043c32cf9fa25698dc79c51c2b
Kenneth Chen <kenchen0606@gmail.com>
2021-11-17 23:22:03 +0000

    load tweet from client.js to id tweets-container

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,85 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const renderTweets = function(tweets) {
+  // loops through tweets
+  // calls createTweetElement for each tweet
+  // takes return value and appends it to the tweets container
+  
+  //$('#tweets-container').empty();
+  for(tweet of tweets) {
+    const $tweet = createTweetElement(tweet);
+    //console.log($tweet);
+    $('#tweets-container').append($tweet); 
+  }
+
+}
+
+const createTweetElement = function(tweet) {
+  let $tweet = `
+    <article class="tweet-content">
+      <header class="user">
+        <div class="user-profile">
+          <img class="user-avatars" src="${tweet.user.avatars}">
+          <a>${tweet.user.name}</a> 
+        </div>
+        <a class="userhandle">${tweet.user.handle}</a>
+      </header>
+      <p>${tweet.content.text}</p>
+      <footer class="d-and-i">
+        <a>${timeago.format(tweet.created_at)}</a>
+        <div class="icons">
+          <i class="fa-solid fa-flag"></i>
+          <i class="fa-solid fa-retweet"></i>
+          <i class="fa-solid fa-heart"></i>
+        </div>
+      </footer>
+    </article>
+  `;
+  // ...
+  return $tweet;
+}
+
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
+$( document ).ready(function() {
+  // const $tweet = createTweetElement(tweetData);
+
+  // // Test / driver code (temporary)
+  // console.log($tweet); // to see what it looks like
+
+  renderTweets(data);
+  //$('#tweets-container').append($tweet); 
+  // Handler for .ready() called.
+  //console.log("test hello");
+  let tweetdb = '/tweets';
+
+  $.get(tweetdb).then(data => {
+    console.log("what happened?");
+    console.log(data);
+  });
+
+});
